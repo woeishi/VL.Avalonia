2a1decd4a670ff950b0031a4aafddecdf30e7482
woeishi <woei@refect.net>
2023-07-11 19:52:11 +0200

    testing context, working not blocking renderer

--- src/AppBuilderExtensions.cs
+++ src/AppBuilderExtensions.cs
@@ -8,13 +8,31 @@ namespace VL.Avalonia
         {
             if (appBuilder != null && setup != null)
             {
+                //var completion = new TaskCompletionSource();
                 var scheduler = TaskScheduler.FromCurrentSynchronizationContext();
                 Task
                     .Run(() => { /* give the host time to resume its mainloop */})
                     .ContinueWith(
-               (x) => { setup.Invoke(appBuilder); }
+               (x) => {
+                   //appBuilder = appBuilder.AfterSetup((ab) => {
+                   //    completion.SetResult();
+                   //});
+                   setup.Invoke(appBuilder); 
+               }
                , scheduler);
+                // something like this, but not blocking to await the setup
+                //completion.Task.Wait();
             }
+           
         }
+
+        public static AppBuilder ConfigureDefault() => AppBuilder.Configure<Application>();
+
+        public static AppBuilder PassThrough(this AppBuilder appBuilder)
+        {
+            Console.WriteLine("here we are");
+            return appBuilder;
+        }
+       
     }
 }
