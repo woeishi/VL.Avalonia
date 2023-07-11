63ecb9e13109b791ca45f25a332429906e966e0f
woeishi <woei@refect.net>
2023-07-11 16:59:50 +0200

    add AppBuilder Setup/Start runner

---
+++ csharp/VL.Avalonia/AppBuilderExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Avalonia;
+
+namespace VL.Avalonia
+{
+    public static class AppBuilderExtensions
+    {
+        public static void RunSetup(this AppBuilder appBuilder, Action<AppBuilder> setup)
+        {
+            if (appBuilder != null && setup != null)
+            {
+                var scheduler = TaskScheduler.FromCurrentSynchronizationContext();
+                Task
+                    .Run(() => { /* give the host time to resume its mainloop */})
+                    .ContinueWith(
+               (x) => { setup.Invoke(appBuilder); }
+               , scheduler);
+            }
+        }
+    }
+}
