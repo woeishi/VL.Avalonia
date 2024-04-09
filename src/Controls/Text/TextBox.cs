f4c42dcf8b9bb2c1fde25e82d2fa67397f76f6a0
woeishi <woei@refect.net>
2024-04-09 11:39:43 +0200

    add minimal impl text controls

---
+++ src/Controls/Text/TextBox.cs
@@ -0,0 +1,63 @@
+﻿using AvaloniaControls = Avalonia.Controls;
+using AvaloniaMedia = Avalonia.Media;
+
+namespace VL.Avalonia.Controls;
+
+[ProcessNode]
+public class TextBox : AvaloniaControls.TextBox
+{
+    protected override Type StyleKeyOverride => typeof(AvaloniaControls.TextBox);
+    public TextBox Output => this;
+
+    public new string? Text
+    {
+        get => base.Text;
+        set { if (value != null && base.Text != value) base.Text = value; }
+    }
+
+    public new AvaloniaMedia.FontFamily FontFamily
+    {
+        get => base.FontFamily;
+        set => base.FontFamily = value;
+    }
+
+    public new double FontSize
+    {
+        get => base.FontSize;
+        set => base.FontSize = value;
+    }
+
+    public new AvaloniaMedia.FontWeight FontWeight
+    {
+        get => base.FontWeight;
+        set => base.FontWeight = value;
+    }
+
+    public new AvaloniaMedia.FontStyle FontStyle
+    {
+        get => base.FontStyle;
+        set => base.FontStyle = value;
+    }
+
+    public new AvaloniaMedia.TextAlignment TextAlignment
+    {
+        get => base.TextAlignment;
+        set => base.TextAlignment = value;
+    }
+
+    public new AvaloniaMedia.TextWrapping TextWrapping
+    {
+        get => base.TextWrapping;
+        set => base.TextWrapping = value;
+    }
+
+    public new string? Watermark
+    {
+        get => base.Watermark;
+        set
+        {
+            if (value != null)
+                base.Watermark = value;
+        }
+    }
+}
