0ac24aa531b522fc0859f0eb2f8e2f97ae8b2ff4
antokhio <antokhio@gmail.com>
2025-05-12 17:24:06 +0300

    added slider prototype, changed names of wrapped components from test to prototype, made styles avalible under Avalonia.Styles

--- src/VL.Avalonia/src/Controls/StackPanelWrapper.cs
+++ src/VL.Avalonia/src/Controls/StackPanelWrapper.cs
@@ -1,50 +1,51 @@
 ﻿using Avalonia.Controls;
-using VL.Avalonia.Styles;
 using VL.Core.Import;
 using VL.Lib.Collections;
+using static VL.Avalonia.Styles;
 
 namespace VL.Avalonia.Controls;
 
-[ProcessNode(Name = "StackPanelTest")]
-public partial class StackPanelWrapper
+[ProcessNode(Name = "StackPanelPrototype")]
+public partial class StackPanelWrapper : AbstractWrapperBase<StackPanel>
 {
-    private StackPanel _control = new StackPanel();
-
-    [Pin(Name = "Output")]
-    public StackPanel Control => _control;
-
+    private readonly StackPanel _output = new StackPanel();
     public StackPanelWrapper()
     {
     }
 
-    private IAvaloniaStyle? _style;
-    public IAvaloniaStyle? Style
-    {
-        set
-        {
-            if (!_style?.Equals(value) ?? _style != value)
-            {
-                _style = value;
-                _style?.ApplyStyle(_control);
-            }
-        }
-    }
+    private Spread<Control> _children;
 
-    private Spread<object> _children;
-    public void SetChildren(Spread<object> children)
+    public override StackPanel Output => _output;
+
+    public void SetChildren(Spread<Control> children)
     {
         if (_children != children)
         {
             _children = children;
-            _control.Children.Clear();
+            _output.Children.Clear();
             foreach (var child in _children)
             {
                 if (child is Control control)
                 {
-                    _control.Children.Add(control);
+                    _output.Children.Add(control);
                 }
             }
         }
     }
 
+    private IAvaloniaStyle? _style;
+    public override IAvaloniaStyle? Style
+    {
+        set
+        {
+            if (!_style?.Equals(value) ?? _style != value)
+            {
+                // Need to test ResetStyle
+                //_output.ApplyStyling();
+
+                _style = value;
+                _style?.ApplyStyle(Output);
+            }
+        }
+    }
 }
