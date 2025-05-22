76ff93d3c3b3e82b7813914a10ca6d46c0b0894a
antokhio <antokhio@gmail.com>
2025-05-22 11:20:19 +0300

    removed wrapper postfix from files, added enabled on button

---
+++ src/VL.Avalonia/src/Controls/Button.cs
@@ -0,0 +1,58 @@
+﻿using Avalonia.Controls;
+using System.Reactive;
+using VL.Avalonia.Attributes;
+using VL.Avalonia.Helpers;
+using VL.Core;
+using VL.Core.Import;
+using VL.Lib.Reactive;
+using static VL.Avalonia.Styles;
+
+namespace VL.Avalonia.Controls;
+
+/// <summary>
+/// The button is a control that reacts to pointer actions (and has some keyboard equivalents). It presents visual feedback in the form of a depressed state when the pointer is down.
+/// </summary>
+[ProcessNode(Name = "Button")]
+public partial class ButtonWrapper
+{
+    [ImplementOutput]
+    private readonly Button _output = new Button();
+
+    [ImplementStyle]
+    private Optional<IAvaloniaStyle> _style;
+
+    [ImplementContent]
+    private Optional<object?> _content;
+
+    protected ChannelCommand<Unit> _command = new((s, a) => new Unit());
+    protected Optional<IChannel<Unit>> _commandChannel;
+    public void SetCommandChannel(Optional<IChannel<Unit>> commandChannel)
+    {
+        if (_commandChannel != commandChannel)
+        {
+            _commandChannel = commandChannel;
+            _output.Command = _command;
+
+            if (_commandChannel.HasValue)
+            {
+                _command.Channel = commandChannel.Value;
+            }
+        }
+    }
+
+    private Optional<ClickMode> _clickMode;
+    public void SetClickMode(Optional<ClickMode> clickMode)
+    {
+        if (_clickMode != clickMode)
+        {
+            _clickMode = clickMode;
+            _output.SetValue(Button.ClickModeProperty, clickMode.Value);
+        }
+    }
+
+    [ImplementIsEnabled<Button>]
+    protected Optional<bool> _isEnabled;
+}
+
+
+
