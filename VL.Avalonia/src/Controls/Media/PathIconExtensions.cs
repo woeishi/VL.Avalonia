917ebebee3f99b9c123aff585eea2cce1e213604
antokhio <antokhio@gmail.com>
2026-09-04 20:12:54 +0300

    addeds set data extensions so can be used in datatemplate, upodates pathicon base

---
+++ VL.Avalonia/src/Controls/Media/PathIconExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia.Controls;
+using Avalonia.Media;
+
+namespace VL.Avalonia.Controls.Media
+{
+    public static class PathIconExtensions
+    {
+        /// <inheritdoc cref="PathIcon.Data"/>
+        public static void SetData(this PathIcon input, Geometry geometry)
+        {
+            if (input is PathIcon pathIcon)
+                pathIcon.Data = geometry;
+        }
+
+        /// <inheritdoc cref="PathIcon.Data"/>
+        public static Geometry? Data(this PathIcon input) => input?.Data;
+    }
+}
+
