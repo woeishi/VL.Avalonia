751c2ac8146a42ec71d5cabb240f9548a79de75e
antokhio <antokhio@gmail.com>
2026-09-04 21:26:53 +0300

    failed binding refactor

--- VL.Avalonia/src/Data/BindingNameNode.cs
+++ VL.Avalonia/src/Data/BindingNameNode.cs
@@ -1,38 +1,78 @@
 ﻿using Avalonia;
 using VL.Core.Import;
-
-// TODO: REFACTOR
+using VL.Lib.Reactive;
 
 namespace VL.Avalonia.Data
 {
-    [ProcessNode(Name = "Binding (Name)")]
-    public class BindingNameNode<TControl, TValue> : BindingNode<TControl, AvaloniaProperty, TValue>
+    /// <summary>
+    /// Binds an <see cref="IChannel{TValue}"/> to an Avalonia property looked up by name on the
+    /// input control, optionally converting between the channel value type and the property value type.
+    /// </summary>
+    [ProcessNode(Name = "Binding (Name Converters)")]
+    public class BindingNameAdvancedNode<TControl, TValue, TProperty>
+        : BindingNode<TControl, AvaloniaProperty, TValue, TProperty>
         where TControl : AvaloniaObject
     {
-        private string _propertyName;
+        private string? _propertyName;
+
+        [Fragment]
+        public BindingNameAdvancedNode(
+            [Pin(Visibility = Model.PinVisibility.Optional)]
+                Func<TValue?, TProperty?>? valueToProperty,
+            [Pin(Visibility = Model.PinVisibility.Optional)]
+                Func<TProperty?, TValue?>? propertyToValue
+        )
+            : base(valueToProperty, propertyToValue) { }
 
         [Fragment(Order = PinOrder.Main)]
-        public void SetProperty(string propertyName)
+        public void SetProperty(string? propertyName)
         {
             if (_propertyName != propertyName)
             {
                 _propertyName = propertyName;
 
-                var inputType = _input?.GetType();
-
-                if (inputType != null)
-                {
-                    var prop = AvaloniaPropertyRegistry.Instance.FindRegistered(
-                        inputType,
-                        propertyName
-                    );
-
-                    if (prop != null)
-                    {
-                        SetProperty(prop);
-                    }
-                }
+                if (ResolveProperty())
+                    Bind();
             }
         }
+
+        protected override void OnInputChanged()
+        {
+            ResolveProperty();
+        }
+
+        /// <summary>
+        /// Resolves the current property name against the current input's type.
+        /// </summary>
+        /// <returns>True if the resolved property changed.</returns>
+        private bool ResolveProperty()
+        {
+            var inputType = _input?.GetType();
+
+            if (inputType is null || string.IsNullOrEmpty(_propertyName))
+                return false;
+
+            var prop = AvaloniaPropertyRegistry.Instance.FindRegistered(inputType, _propertyName);
+
+            if (prop is null || prop == _property)
+                return false;
+
+            _property = prop;
+
+            return true;
+        }
+    }
+
+    /// <summary>
+    /// A <see cref="BindingNameAdvancedNode{TControl, TValue, TProperty}"/> for the common case where the
+    /// Avalonia property value type and the channel value type are the same.
+    /// </summary>
+    [ProcessNode(Name = "Binding (Name)")]
+    public class BindingNameNode<TControl, TValue> : BindingNameAdvancedNode<TControl, TValue, TValue>
+        where TControl : AvaloniaObject
+    {
+        [Fragment]
+        public BindingNameNode()
+            : base(null, null) { }
     }
 }
--- VL.Avalonia/src/Data/BindingNode.cs
+++ VL.Avalonia/src/Data/BindingNode.cs
@@ -1,10 +1,10 @@
-﻿using System.Reactive.Disposables;
+﻿using Avalonia;
+using System.Globalization;
+using System.Reactive.Disposables;
 using System.Reactive.Linq;
-using Avalonia;
+using System.Runtime.CompilerServices;
 using VL.Core.Import;
 using VL.Lib.Reactive;
-
-// TODO: REFACTOR
 
 namespace VL.Avalonia.Data
 {
@@ -18,20 +18,28 @@ namespace VL.Avalonia.Data
     {
         None,
 
-        /// <summary>Sets channel value to control before bind</summary>
+        /// <summary>
+        /// Sets channel value to control before bind
+        /// </summary>
         ChannelToControl,
 
-        /// <summary>Control value to channel before bind</summary>
+        /// <summary>
+        /// Control value to channel before bind
+        /// </summary>
         ControlToChannel,
     }
 
-    [ProcessNode(Name = "Binding", FragmentSelection = FragmentSelection.Explicit)]
-    public abstract class BindingNode<TControl, TProp, TValue> : IDisposable
+    /// <summary>
+    /// Binds an <see cref="IChannel{TValue}"/> to an Avalonia property of type <typeparamref name="TProperty"/>,
+    /// optionally converting between the channel value type and the property value type.
+    /// </summary>
+    [ProcessNode(Name = "Binding (Converters)", FragmentSelection = FragmentSelection.Explicit)]
+    public class BindingNode<TControl, TAvaloniaProperty, TValue, TProperty> : IDisposable
         where TControl : AvaloniaObject
-        where TProp : AvaloniaProperty
+        where TAvaloniaProperty : AvaloniaProperty
     {
         protected TControl? _input;
-        protected TProp? _property;
+        protected TAvaloniaProperty? _property;
         protected SupprotedBindingInitialValueHandling _initialValueHandling =
             SupprotedBindingInitialValueHandling.ChannelToControl;
         protected IChannel<TValue>? _channel;
@@ -39,10 +47,22 @@ namespace VL.Avalonia.Data
 
         protected SupportedBindingMode _mode = SupportedBindingMode.OneWay;
 
-        private CompositeDisposable _subscriptions = new();
+        protected readonly Func<TValue?, TProperty?>? _valueToProperty;
+        protected readonly Func<TProperty?, TValue?>? _propertyToValue;
+
+        private readonly CompositeDisposable _subscriptions = new();
 
         [Fragment]
-        public BindingNode() { }
+        public BindingNode(
+            [Pin(Visibility = Model.PinVisibility.Optional)]
+                Func<TValue?, TProperty?>? valueToProperty,
+            [Pin(Visibility = Model.PinVisibility.Optional)]
+                Func<TProperty?, TValue?>? propertyToValue
+        )
+        {
+            _valueToProperty = valueToProperty;
+            _propertyToValue = propertyToValue;
+        }
 
         [Fragment(Order = PinOrder.Main)]
         public void SetInput(TControl? input)
@@ -52,13 +72,12 @@ namespace VL.Avalonia.Data
 
             _input = input;
 
-            if (_input is not null)
-            {
-                Bind();
-            }
+            OnInputChanged();
+
+            Bind();
         }
 
-        public void SetProperty(TProp property)
+        public void SetProperty(TAvaloniaProperty? property)
         {
             if (_property != property)
             {
@@ -104,68 +123,148 @@ namespace VL.Avalonia.Data
             }
         }
 
+        /// <summary>
+        /// Called after <see cref="_input"/> changed and before the binding is re-established.
+        /// </summary>
+        protected virtual void OnInputChanged() { }
+
+        /// <summary>
+        /// Converts a channel value into a property value, using the optional converter.
+        /// </summary>
+        protected TProperty? ToProperty(TValue? value)
+        {
+            if (_valueToProperty is not null)
+                return _valueToProperty(value);
+
+            if (typeof(TValue) == typeof(TProperty))
+                return Unsafe.As<TValue?, TProperty?>(ref value);
+
+            return Convert<TProperty>(value);
+        }
+
+        /// <summary>
+        /// Converts a property value into a channel value, using the optional converter.
+        /// </summary>
+        protected TValue? ToValue(TProperty? value)
+        {
+            if (_propertyToValue is not null)
+                return _propertyToValue(value);
+
+            if (typeof(TValue) == typeof(TProperty))
+                return Unsafe.As<TProperty?, TValue?>(ref value);
+
+            return Convert<TValue>(value);
+        }
+
+        /// <summary>
+        /// Converts a channel value into a boxed property value, boxing at most once.
+        /// </summary>
+        protected object? ToPropertyObject(TValue? value) => ToProperty(value);
+
+        /// <summary>
+        /// Converts an already boxed property value into a channel value, without re-boxing.
+        /// </summary>
+        protected TValue? FromPropertyObject(object? value) =>
+            _propertyToValue is null && typeof(TValue) != typeof(TProperty)
+                ? Convert<TValue>(value)
+                : ToValue(Convert<TProperty>(value));
+
+        /// <summary>
+        /// Converts a value of an unrelated type without throwing. Returns the default value when no
+        /// conversion is possible - a converter should be supplied for those cases.
+        /// </summary>
+        private static TTarget? Convert<TTarget>(object? value)
+        {
+            if (value is null)
+                return default;
+
+            if (value is TTarget target)
+                return target;
+
+            try
+            {
+                var targetType =
+                    Nullable.GetUnderlyingType(typeof(TTarget)) ?? typeof(TTarget);
+
+                return (TTarget?)
+                    System.Convert.ChangeType(value, targetType, CultureInfo.InvariantCulture);
+            }
+            catch (Exception e)
+                when (e
+                        is InvalidCastException
+                            or FormatException
+                            or OverflowException
+                            or ArgumentException
+                )
+            {
+                return default;
+            }
+        }
+
         protected virtual void Bind()
         {
-            _subscriptions?.Clear();
+            _subscriptions.Clear();
 
-            if (_input != null && _property != null)
+            if (_input is null || _property is null)
+                return;
+
+            var channel = _channel ?? _internalChannel;
+
+            if (channel is null)
+                return;
+
+            // Initial value handling
+
+            if (_initialValueHandling is SupprotedBindingInitialValueHandling.ChannelToControl)
             {
-                var channel = (_channel ?? _internalChannel);
+                // Set from channel to control
+                _input.SetValue(_property, ToPropertyObject(channel.Value));
+            }
+            if (_initialValueHandling is SupprotedBindingInitialValueHandling.ControlToChannel)
+            {
+                //  Set from control to channel
+                channel.SetValue(FromPropertyObject(_input.GetValue(_property)));
+            }
 
-                if (channel != null)
-                {
-                    // Initial value handling
+            var channelToBind = channel.Select(ToPropertyObject);
 
-                    if (
-                        _initialValueHandling
-                        is SupprotedBindingInitialValueHandling.ChannelToControl
-                    )
-                    {
-                        // Set from channel to control
-                        _input.SetValue(_property, channel.Value);
-                    }
-                    if (
-                        _initialValueHandling
-                        is SupprotedBindingInitialValueHandling.ControlToChannel
-                    )
-                    {
-                        var pv = _input.GetValue(_property);
-                        //  Set from control to channel
-                        channel.SetValue((TValue?)pv);
-                    }
+            _subscriptions.Add(_input.Bind(_property, channelToBind));
 
-                    var channelToBind = channel.Select(x => (object?)x);
+            switch (_mode)
+            {
+                case SupportedBindingMode.TwoWay:
+                    var observable = _input.GetObservable(_property).Skip(1);
 
-                    _subscriptions?.Add(_input.Bind(_property, channelToBind));
+                    _subscriptions.Add(
+                        observable.Subscribe(x => channel.OnNext(FromPropertyObject(x)!))
+                    );
 
-                    switch (_mode)
-                    {
-                        case SupportedBindingMode.TwoWay:
-                            var observable = _input.GetObservable(_property).Skip(1);
-
-                            _subscriptions?.Add(
-                                observable.Subscribe(x =>
-                                {
-                                    if (x is TValue v)
-                                        channel.OnNext(v);
-                                    else if (x is null)
-                                        channel.OnNext(default!);
-                                })
-                            );
-
-                            return;
-                        case SupportedBindingMode.OneWay:
-                        default:
-                            return;
-                    }
-                }
+                    return;
+                case SupportedBindingMode.OneWay:
+                default:
+                    return;
             }
         }
 
         public void Dispose()
         {
-            _subscriptions?.Clear();
+            _subscriptions.Clear();
             _internalChannel?.Dispose();
         }
+    }
+
+    /// <summary>
+    /// A <see cref="BindingNode{TControl, TAvaloniaProperty, TValue, TProperty}"/> for the common case
+    /// where the Avalonia property value type and the channel value type are the same.
+    /// </summary>
+    [ProcessNode(Name = "Binding", FragmentSelection = FragmentSelection.Explicit)]
+    public class BindingNode<TControl, TAvaloniaProperty, TValue>
+        : BindingNode<TControl, TAvaloniaProperty, TValue, TValue>
+        where TControl : AvaloniaObject
+        where TAvaloniaProperty : AvaloniaProperty
+    {
+        [Fragment]
+        public BindingNode()
+            : base(null, null) { }
     }
 }
